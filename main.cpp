885e234bbc99bb88fbeaff0ac204b84617c9e6aa
BlackWoodik <100531159+BlackWoodik@users.noreply.github.com>
2022-06-05 21:41:40 +0300

    shag 3.2.8 -start

--- main.cpp
+++ main.cpp
@@ -90,7 +90,21 @@ show_histogram_text(const auto bins){
 }
 
 int
-main() {
+main(int argc, char* argv[]) {
+    if (argc>1){
+        CURL *curl = curl_easy_init();
+        if(curl) {
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform(curl);
+            if (res!=0){
+                curl_easy_strerror(res);
+                exit(1);
+                }
+            curl_easy_cleanup(curl);
+        }
+        return 0;
+    }
     curl_global_init(CURL_GLOBAL_ALL);
     const auto input = read_input(cin, true);
     const auto bins = make_histogram(input);
