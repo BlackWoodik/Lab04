22ee8614be2029eae5ef064f94cd4ea70cb39881
BlackWoodik <100531159+BlackWoodik@users.noreply.github.com>
2022-06-06 12:05:11 +0300

    Lab04-End

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <sstream>
+#include <string>
 #include <iostream>
 #include <vector>
 #include <curl/curl.h>
@@ -89,13 +91,14 @@ show_histogram_text(const auto bins){
     }
 }
 
-int
-main(int argc, char* argv[]) {
-    if (argc>1){
-        CURL *curl = curl_easy_init();
+Input
+download(const string& address) {
+    stringstream buffer;
+
+    CURL *curl = curl_easy_init();
         if(curl) {
             CURLcode res;
-            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
             res = curl_easy_perform(curl);
             if (res!=0){
                 curl_easy_strerror(res);
@@ -103,10 +106,27 @@ main(int argc, char* argv[]) {
                 }
             curl_easy_cleanup(curl);
         }
-        return 0;
+
+    return read_input(buffer, false);
+}
+
+size_t
+write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
+    size_t data_size = item_size * item_count;
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    buffer->write(reinterpret_cast<const char*>(items), data_size);
+    return data_size;
+}
+
+int
+main(int argc, char* argv[]) {
+    Input input;
+    if (argc > 1) {
+        input = download(argv[1]);
+    } else {
+        input = read_input(cin, true);
     }
-    curl_global_init(CURL_GLOBAL_ALL);
-    const auto input = read_input(cin, true);
+
     const auto bins = make_histogram(input);
     show_histogram_svg(bins);
 }
